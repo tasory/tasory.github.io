8e8d6de484e6398b497d385312f2dc4c2d340e8e
Tasory <83129333+tasory@users.noreply.github.com>
2025-05-28 02:10:55 +0400

    Add files via upload

---
+++ lastfm.js
@@ -0,0 +1,33 @@
+
+// vars
+const apiKey = '4c6725131b213c8d0955896b938a34b0';
+const username = 'tasoryy';
+let currentTrackName = '';
+
+// script
+function fetchCurrentTrack() {
+    fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json`)
+        .then(response => response.json())
+        .then(data => {
+            const recentTracks = data.recenttracks.track;
+            if (recentTracks.length > 0) {
+                const currentTrack = recentTracks[0];
+                const newTrackName = currentTrack.name;
+
+                if (newTrackName !== currentTrackName) {
+                    currentTrackName = newTrackName;
+                    document.getElementById('album-title').innerText = currentTrack.album['#text'];
+                    document.getElementById('track-title').innerText = currentTrack.name;
+                    document.getElementById('artist-name').innerText = currentTrack.artist['#text'];
+                    document.getElementById('album-cover').src = currentTrack.image[3]['#text'];
+                }
+            } else {
+                document.getElementById('album-title').innerText = 'No data';
+                document.getElementById('track-title').innerText = '';
+            }
+        })
+        .catch(error => console.error('error:', error));
+}
+
+setInterval(fetchCurrentTrack, 1000);
+fetchCurrentTrack();
